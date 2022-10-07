d1cfb6bd378b22c60de1c5858e6fe6b21366f608
Piotr Migaj praca <piotr.migaj@pg-projekt.pl>
2022-10-07 06:48:22 +0200

    next commit

--- src/main/java/pl/migibud/studentApp/StudentAppApplication.java
+++ src/main/java/pl/migibud/studentApp/StudentAppApplication.java
@@ -2,14 +2,10 @@ package pl.migibud.studentApp;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import pl.migibud.studentApp.model.InitialCourseData;
 
 @Slf4j
 @SpringBootApplication
